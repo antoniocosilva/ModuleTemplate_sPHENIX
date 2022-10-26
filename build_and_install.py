be36e260d7d9908e5c7e69e3c0173e6e90aeadac
Antonio Carlos Oliveira da Silva <antoniosilva@sphnx01.sdcc.bnl.gov>
2022-10-26 11:14:36 -0400

    Adding build script

---
+++ build_and_install.py
@@ -0,0 +1,37 @@
+import sys, os
+from os import environ
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser(description='Creates a new sPHENIX module')
+parser.add_argument('-i', '--installDir', type=str, default="MyInstall", help='Name of the intallation directory')
+
+args = parser.parse_args()
+
+userRootDir = os.path.expanduser('~')
+userInstallDir = "{}/{}".format(userRootDir,args.installDir)
+os.makedirs(userInstallDir, exist_ok=True)
+
+myCurrentPath = os.getcwd()
+
+moduleName = os.path.basename(os.path.normpath(myCurrentPath))
+
+search_text = "ModuleTemplate_sPHENIX"
+replace_text = moduleName
+
+with open(myCurrentPath+"/Fun4All_Template.C", 'r') as file:
+    data = file.read()
+    data = data.replace(search_text, moduleName)
+    data = data.replace("/path_to_user_install", userInstallDir)
+with open(myCurrentPath+"/Fun4All_Template.C", 'w') as file:
+    file.write(data)
+
+buildDir = myCurrentPath + "/src/build"
+
+print(buildDir)
+
+os.makedirs(buildDir, exist_ok=True)
+
+os.chdir(buildDir)
+
+subprocess.call(['sh', myCurrentPath + "/src/autogen.sh --prefix=" + userInstallDir])
