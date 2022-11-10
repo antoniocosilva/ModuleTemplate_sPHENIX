0bedd63010b2da6ef5e9142a718391ae3da1ff9e
Antonio Silva <54371755+antoniocosilva@users.noreply.github.com>
2022-11-10 13:49:31 -0500

    Update build_and_install.py
    Make autogen.sh executable

--- build_and_install.py
+++ build_and_install.py
@@ -37,6 +37,8 @@ os.chdir(buildDir)
 
 print(myCurrentPath)
 
+os.chmod(myCurrentPath + "/src/autogen.sh, stat.S_IXUSR)
+
 #subprocess.call(['sh', myCurrentPath + "/src/autogen.sh --prefix=" + userInstallDir])
 autogen = myCurrentPath + "/src/autogen.sh --prefix=" + userInstallDir
 os.system(autogen)
